22f4d7d14d4b62d39c8778f05288eedadf5f88ff
Edtor <mj.purple42@gmail.com>
2026-09-10 21:00:31 +0200

    first working variations generation

--- RandomApp/VariationsPage.xaml.cs
+++ RandomApp/VariationsPage.xaml.cs
@@ -5,5 +5,20 @@ public partial class VariationsPage : ContentPage
 	public VariationsPage()
 	{
 		InitializeComponent();
-	}
+		Random r = new Random();
+    }
+	public void OnGenerateClicked(object sender, EventArgs e)
+    {
+		Random r = new Random();
+		int min = int.Parse(minEntry.Text);
+		int max = int.Parse(maxEntry.Text) + 1;
+		int amount = int.Parse(amountEntry.Text);
+		resultLabel.Text = string.Empty;
+		for(int i = 0; i < amount; i++)
+		{
+			int result = r.Next(min, max);
+			resultLabel.Text += result;
+			if (i + 1 < amount) resultLabel.Text += ", ";
+		}
+    }
 }
